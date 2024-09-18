2c259630d5e1192a0b15243a0bc543390728b251
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2024-09-18 20:26:00 +0530

    Make Hero section buttons clickable; implement Parallax section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Test from "./Test.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Hero from "./components/hero/Hero.jsx";
+import Parallax from "./components/parallax/Parallax.jsx";
 
 const App = () => {
   return <div>
@@ -9,9 +10,9 @@ const App = () => {
       <Navbar />
       <Hero />  
     </section>
-    <section id="Services">parallax</section>
+    <section id="Services"><Parallax type="services"/></section>
     <section>Services</section>
-    <section id="Portfolio">Parallax</section>
+    <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -16,7 +16,7 @@ const textVariants = {
     },
     scrollButton: {
         opacity: 0,
-        y: 10,
+        y: [0, 10, 0],
         transition: {
             duration: 1.5,
             repeat: Infinity,
@@ -38,13 +38,16 @@ const sliderVariants = {
     },
 };
 
+const handleResumeClick = () => {
+    window.open("https://drive.google.com/file/d/1BShRL7mjSt6XXpnL9oNu-uKdoCSCHIed/view?usp=drive_link", "_blank", "noopener,noreferrer");
+};
+
 const Hero = () => {
     return (
         <div className="hero">
-
             <div className="wrapper">
-                <motion.div 
-                    className="textContainer" 
+                <motion.div
+                    className="textContainer"
                     variants={textVariants}
                     initial="initial"
                     animate="animate"
@@ -54,9 +57,15 @@ const Hero = () => {
                     <motion.div variants={textVariants} className="buttons">
                         <motion.button variants={textVariants}>See the Latest Works</motion.button>
                         <motion.button variants={textVariants}>Contact Me</motion.button>
+                        <motion.button 
+                            variants={textVariants} 
+                            onClick={handleResumeClick}
+                        >
+                            My Resume
+                        </motion.button>
                     </motion.div>
                     <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt=""></motion.img>
-                </motion.div>               
+                </motion.div>
             </div>
 
             <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
@@ -66,7 +75,6 @@ const Hero = () => {
             <div className="imageContainer">
                 <img src="/hero.png" alt=""></img>
             </div>
-
         </div>
     );
 };
